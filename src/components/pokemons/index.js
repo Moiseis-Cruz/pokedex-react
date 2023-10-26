3a0dc5e29b243db57d6dc8012622e98e8ea5ede0
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-10-25 22:51:52 -0300

    :recycle: refactor: index.js

--- src/components/pokemons/index.js
+++ src/components/pokemons/index.js
@@ -1,37 +1,26 @@
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 import styled from "styled-components";
 import { ThemeContext } from "../../theme-context/theme-context";
-
-async function pokemonList() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-    const pokemons = await response.json()
-    return pokemons.results
-}
+import { usePokemonHook } from "../../hooks/usePokemonHook";
 
 export const ListCards = () => {
-    const [list, setList] = useState([])
-
-    useEffect(()=>{
-        async function fetchData() {
-            const pokeId = await pokemonList()
-            
-            setList(pokeId)
-        }
-        
-        fetchData()
-    },[])
-
+    const { pokemonData } = usePokemonHook()
+    console.log(pokemonData)
     const { theme } = useContext(ThemeContext)
 
-    return(
-        <Main style={{color: theme.color, backgroundColor: theme.backgroundColor}}>
+    return (
+        <Main style={{ color: theme.color, backgroundColor: theme.backgroundColor }}>
             <ul>
                 {
-                    list.map((item, index) => {
-                        return(
+                    pokemonData && pokemonData.map((item, index) => {
+                        return (
                             <div key={index}>
-                                <img src={item.url} alt={item.name} />
+                                <img src={item.sprites.front_shiny
+                                } alt={item.name} />
                                 <h2>{item.name}</h2>
+                                <div className="types">{item.types.map((item) => (
+                                    <p className={item.type.name}>{item.type.name}</p>
+                                ))}</div>
                             </div>
                         )
                     })
@@ -46,6 +35,145 @@ const Main = styled.main`
     width: 100%;
     background-color: pink;
     margin: auto;
+
+    
+    .types {
+        display: flex;
+        flex-direction: row;
+        gap: 5px;
+    }
+
+    .types p {
+        padding: 5px;
+        border-radius: 5px;
+    }
+
+    .fire {
+        color: red;
+    }
+
+    .water {
+        color: blue;
+    }
+
+    ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    div {
+        padding: 20px;
+        width: 200px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    /* Fogo */
+    .fire {
+    color: white;
+    background-color: #FF4500; /* Laranja escuro */
+    }
+
+    /* Água */
+    .water {
+    color: white;
+    background-color: #0077BE; /* Azul-claro */
+    }
+
+    /* Grama */
+    .grass {
+    color: white;
+    background-color: #4CAF50; /* Verde */
+    }
+
+    /* Elétrico */
+    .electric {
+    color: black;
+    background-color: #FFD700; /* Amarelo */
+    }
+
+    /* Gelo */
+    .ice {
+    color: black;
+    background-color: #87CEEB; /* Azul-claro */
+    }
+
+    /* Lutador */
+    .fighting {
+    color: white;
+    background-color: #A0522D; /* Marrom */
+    }
+
+    /* Veneno */
+    .poison {
+    color: white;
+    background-color: #800080; /* Roxo */
+    }
+
+    /* Terra */
+    .ground {
+    color: black;
+    background-color: #8B4513; /* Marrom-escuro */
+    }
+
+    /* Voador */
+    .flying {
+    color: black;
+    background-color: #87CEEB; /* Azul-claro */
+    }
+
+    /* Psíquico */
+    .psychic {
+    color: white;
+    background-color: #FF1493; /* Rosa */
+    }
+
+    /* Inseto */
+    .bug {
+    color: black;
+    background-color: #32CD32; /* Verde-claro */
+    }
+
+    /* Pedra */
+    .rock {
+    color: white;
+    background-color: #A9A9A9; /* Cinza */
+    }
+
+    /* Fantasma */
+    .ghost {
+    color: white;
+    background-color: #4B0082; /* Indigo escuro */
+    }
+
+    /* Aço */
+    .steel {
+    color: black;
+    background-color: #A9A9A9; /* Cinza */
+    }
+
+    /* Dragão */
+    .dragon {
+    color: white;
+    background-color: #4169E1; /* Azul */
+    }
+
+    /* Sombrio */
+    .dark {
+    color: white;
+    background-color: #2F4F4F; /* Cinza-escuro */
+    }
+
+    /* Fada */
+    .fairy {
+    color: black;
+    background-color: #EE82EE; /* Violeta */
+    }
+
 `
 
 /* 
@@ -69,8 +197,8 @@ list.data.map((item, index) => {
     })
     */
 
-    /*
+/*
 
-    https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index+1}.png
+https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index+1}.png
 
-    */
+*/
