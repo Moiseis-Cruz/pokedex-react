a7a4fccd4c8b0fe110a63c722301d182b3bedb80
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-10-25 22:51:26 -0300

    :sparkles: feat: add usePokemonHook

---
+++ src/hooks/usePokemonHook.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+export const usePokemonHook = () => {
+    const [pokemonData, setpokemonData] = useState([]);
+
+    const fetchPokemonList = async () => {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+        const data = await response.json();
+        return data.results;
+    };
+
+    const fetchPokemon = async (url) => {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    };
+
+    const fetchPokemonData = async () => {
+        try {
+            // Primeira requisição
+            const pokemonList = await fetchPokemonList();
+            
+            const pokemonDataPromises = pokemonList.map((pokemon) =>
+                fetchPokemon(pokemon.url)
+            );
+
+            const pokemonData = await Promise.all(pokemonDataPromises);
+
+            setpokemonData(pokemonData);
+        } catch (error) {
+            console.error('Error fetching Pokemon data:', error.message);
+        }
+    };
+
+    useEffect(() => {
+        fetchPokemonData();
+    }, []);
+
+    return {
+        pokemonData
+    }
+}
